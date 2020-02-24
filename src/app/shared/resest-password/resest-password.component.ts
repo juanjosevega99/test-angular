f518c6c6ad633237e92ddce904755c1164dff477
aldairl <aldairl@unicauca.edu.co>
2020-02-24 14:21:27 -0500

    validacion de contraseña

--- src/app/shared/resest-password/resest-password.component.ts
+++ src/app/shared/resest-password/resest-password.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from "@angular/forms"
+import { FormGroup, FormControl, Validators } from "@angular/forms"
 
 @Component({
   selector: 'app-resest-password',
@@ -8,14 +8,54 @@ import { NgForm } from "@angular/forms"
 })
 export class ResestPasswordComponent implements OnInit {
 
-  constructor() { }
+  seepass:boolean=false;
+  Typetext:string='password';
+  seepass1:boolean=false;
+  Typetext1:string='password';
+
+  //forms
+  newpassword: FormGroup;
+
+  constructor() {
+
+    this.newpassword = new FormGroup({
+      'password' : new FormControl('', [Validators.required, Validators.pattern("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}") ]),
+      'verifypassword' : new FormControl()
+    })
+   
+
+   }
 
   ngOnInit() {
+    
   }
 
-  send(forma:NgForm ){
+  send(){
 
-    console.log(forma);
+    console.log(this.newpassword);
   }
+
+  seepassword(){
+    this.seepass = !this.seepass;
+
+    if(!this.seepass){
+      this.Typetext = 'password'
+    }else{
+      this.Typetext='text'
+    }
+  }
+
+  seepassword1(){
+    this.seepass1 = !this.seepass1;
+
+    if(!this.seepass1){
+      this.Typetext1 = 'password'
+    }else{
+      this.Typetext1='text'
+    }
+  }
+
+  //verificar password
+
 
 }
