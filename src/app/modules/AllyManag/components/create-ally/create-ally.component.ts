b426729418787c6d5624f453114dda632aa00a68
MichaelPardo <pardo.burbano@gmail.com>
2020-02-26 15:34:30 -0500

    add fields form and functions

--- src/app/modules/AllyManag/components/create-ally/create-ally.component.ts
+++ src/app/modules/AllyManag/components/create-ally/create-ally.component.ts
@@ -11,25 +11,34 @@ export class CreateAllyComponent implements OnInit {
 
   aliado: Aliado;
   horas: String[] = [];
-  TipoEstablecimiento: String[] = [];
+  TypeEstablishment: String[] = [];
   Categoria: String[] = [];
   photo: any;
   imagesUploaded: any = [];
   imageObject: any;
   imageSize: any
+  //handle button other category
+  otherCatSelect: boolean = true
+  otherCatInput: boolean = false
+  //handle button other type Establishment
+  otherEstablishmentSelect: boolean = true
+  otherEstablishmentInput: boolean = false
+  newEstablishment:string
   constructor() {
     this.imageSize = { width: 230, height: 120 };
     this.aliado = new Aliado();
     this.aliado.colors = ["", "", ""];
     this.aliado.days = [{}, {}, {}, {}, {}, {}, {}]
     this.aliado.images = [];
-    this.Categoria = ["Alta cocina", "Comida rapida", "Comida tipica", "Ensalada y vegatariana", "Ejecutiva",
-      "Comida internacional", "Heladeria", "Cafeteria", "Desayuno", "Hamburguesas"]
-    this.TipoEstablecimiento = ["Alta cocina", "Restaurante tradicional", "Cafeteria", "Restaurante de cadena",
-      "Saludable", "Heladeria"]
+    this.Categoria = ["Alta cocina", "Comida rápida", "Comida típica", "Ensaladas y vegatariana", "Ejecutiva",
+      "Comida internacional", "Heladería", "Cafetería", "Desayuno", "Hamburguesas","Pizzas","Pastas"
+      ,"Perros calientes","Pollo","Árabe","Mariscos","Oriental","Italiana","Mexicana","Postres","Peruana"
+      ,"Sándwich","Arepas y empanadas","Alitas","Crepes","Restaurante bar"]
+    this.TypeEstablishment = ["Alta cocina", "Restaurante tradicional", "Cafetería", "Restaurante de cadena",
+      "Saludable", "Heladería"]
+
     this.horas = ["10:00 am", "11:00 am", "12:00 pm", "01:00 pm", "02:00 pm", "03:00 pm", "04:00 pm", "05:00 pm",
       "06:00 pm", "07:00 pm", "08:00 pm", "09:00 pm", "10:00 pm", "11:00 pm", "12:00 am"]
-
 
   }
 
@@ -67,6 +76,32 @@ export class CreateAllyComponent implements OnInit {
 
       this.aliado.images.push(input.files[0])
       reader.readAsDataURL(input.files[0]);
+    }
+
+  }
+  handleBoxEstablishment():boolean{
+    if (this.otherEstablishmentSelect) {
+        return this.otherEstablishmentSelect = false,
+        this.otherEstablishmentInput= true  
+            
+    }else{
+      return this.otherEstablishmentSelect = true,
+      this.otherEstablishmentInput= false       
+    }
+
+  }
+  addEstablishment(termino:String){
+    this.newEstablishment = termino.toLowerCase();
+    this.TypeEstablishment.push(this.newEstablishment)
+  }
+  handleBoxCategory():boolean{
+    if (this.otherCatSelect) {
+        return this.otherCatSelect = false,
+        this.otherCatInput= true  
+            
+    }else{
+      return this.otherCatSelect = true,
+      this.otherCatInput= false       
     }
 
   }
