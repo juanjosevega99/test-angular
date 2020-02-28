1b51884403d940c2dbd88012fc240959b9cd831d
dianasofi <dianasofi@unicauca.edu.co>
2020-02-28 10:51:40 -0500

    services json

--- src/app/modules/AllyManag/components/create-headquarter/create-headquarter.component.ts
+++ src/app/modules/AllyManag/components/create-headquarter/create-headquarter.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateHeadquarterComponent implements OnInit {
 
+  servicios: any[] = [{name: 'Parqueadero', img:'assets/icons/parking.png' },{name: 'Barra de tragos', img:'assets/icons/cocktail.png' },{name: 'Wifi', img:'assets/icons/wi-fi-zone.png' },
+  {name: 'Mesa exterior', img:'assets/icons/people-table.png' },{name: 'Acceso a discapacitados', img:'assets/icons/discapacity.png' },{name: 'Show en vivo', img:'assets/icons/dance.png' },
+  {name: 'Zona de fumadores', img:'assets/icons/no-smoking.png' },{name: 'Carta braile', img:'assets/icons/braille.png' }]
+
   constructor() { }
 
   ngOnInit() {
