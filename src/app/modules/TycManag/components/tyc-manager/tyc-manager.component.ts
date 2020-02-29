a1c8f0402069ab1c57a3555bcad87d92b5a36c08
aldairl <aldairl@unicauca.edu.co>
2020-02-29 12:21:33 -0500

    implementing usemanager

--- src/app/modules/TycManag/components/tyc-manager/tyc-manager.component.ts
+++ src/app/modules/TycManag/components/tyc-manager/tyc-manager.component.ts
@@ -18,49 +18,49 @@ export class TycManagerComponent implements OnInit {
 
   users = [
     {
-      date: '2020-02-01', name: 'Andrea', email: 'john@example.com', cellphone: '3245672341', bornDate: '09/01/1997',
+      date: '2020-02-01', name: 'Andrea', email: 'john@example.com', cellphone: '3245672341', birthday: '09/01/1997',
       gender: 'femenino', establishment: 'KFC', headquart: 'Galerías', usability: '1', quantity: '$12.000'
     },
 
     {
-      date: '2020-02-02', name: 'Kenny', email: 'mary@mail.com', cellphone: '3125672341', bornDate: '19/01/1995',
+      date: '2020-02-02', name: 'Kenny', email: 'mary@mail.com', cellphone: '3125672341', birthday: '19/01/1995',
       gender: 'femenino', establishment: 'KFC', headquart: 'Centro', usability: '1', quantity: '$12.000'
     },
 
     {
-      date: '2020-02-03', name: 'Ana', email: 'july@greatstuff.com', cellphone: '3214577223', bornDate: '11/05/1945',
+      date: '2020-02-03', name: 'Ana', email: 'july@greatstuff.com', cellphone: '3214577223', birthday: '11/05/1945',
       gender: 'femenino', establishment: 'Corral', headquart: 'Galerías', usability: '1', quantity: '$12.000'
     },
 
     {
-      date: '2020-02-04', name: 'Sofia', email: 'a_r@test.com', cellphone: '3214577223', bornDate: '3105672341',
+      date: '2020-02-04', name: 'Sofia', email: 'a_r@test.com', cellphone: '3214577223', birthday: '1999-03-19',
       gender: 'femenino', establishment: 'Corral', headquart: 'Centro', usability: '1', quantity: '$12.000'
     },
 
     {
-      date: '2019-02-01', name: 'Edwin', email: 'a_r@test.com', cellphone: '3145332122', bornDate: '3105672341',
+      date: '2019-02-01', name: 'Edwin', email: 'a_r@test.com', cellphone: '3145332122', birthday: '2013-02-07',
       gender: 'masculino', establishment: 'Corral', headquart: 'Norte', usability: '0', quantity: '$12.000'
     },
 
     {
-      date: '2018-02-01', name: 'Isabella', email: 'a_r@test.com', cellphone: '3245672341', bornDate: '3105672341',
+      date: '2018-02-01', name: 'Isabella', email: 'a_r@test.com', cellphone: '3245672341', birthday: '1992-01-05',
       gender: 'femenino', establishment: 'Qbano', headquart: 'Galerías', usability: '1', quantity: '$12.000'
     },
 
     {
-      date: '2018-02-05', name: 'kenny', email: 'a_r@test.com', cellphone: '3245672341', bornDate: '3105672341',
+      date: '2018-02-05', name: 'kenny', email: 'a_r@test.com', cellphone: '3245672341', birthday: '1995-06-25',
       gender: 'femenino', establishment: 'Qbano', headquart: 'Galerías', usability: '1', quantity: '$12.000'
     },
 
     {
-      date: '2018-02-05', name: 'kenny', email: 'a_r@test.com', cellphone: '3245672341', bornDate: '3105672341',
+      date: '2018-02-05', name: 'kenny', email: 'a_r@test.com', cellphone: '3245672341', birthday: '1994-05-15',
       gender: 'femenino', establishment: 'kfc', headquart: 'Galerías', usability: '1', quantity: '$12.000'
     },
 
   ]
 
   newdateArray: {
-    date: string, name: string, email: string, cellphone: string, bornDate: string, gender: string, establishment: string,
+    date: string, name: string, email: string, cellphone: string, birthday: string, gender: string, establishment: string,
     headquart: string, usability: string, quantity: string
   }[] = this.users;
 
@@ -151,7 +151,7 @@ export class TycManagerComponent implements OnInit {
   }
 
 
-  search(termino?: string) {
+  search( termino?: string, id?:string ) {
 
     if (this.fromDate && this.toDate) {
 
@@ -166,7 +166,7 @@ export class TycManagerComponent implements OnInit {
         this.newdateArray = [];
 
         aux.forEach(user => {
-          if (user.name.toLowerCase().indexOf(termino) >= 0) {
+          if (user[id].toLowerCase().indexOf(termino) >= 0) {
             this.newdateArray.push(user)
           }
 
@@ -190,7 +190,7 @@ export class TycManagerComponent implements OnInit {
 
         this.users.forEach(user => {
 
-          if (user.name.toLowerCase().indexOf(termino) >= 0) {
+          if (user[id].toLowerCase().indexOf(termino) >= 0) {
             this.newdateArray.push(user)
           }
 
@@ -200,6 +200,23 @@ export class TycManagerComponent implements OnInit {
         this.newdateArray = this.users;
       }
     }
+  }
+
+  searchbyterm(termino:string){
+    console.log(termino);
+    termino = termino.toLowerCase();
+
+    this.newdateArray = [];
+
+    var myRegex = new RegExp('.*' + termino + '.*','gi'); 
+
+    this.newdateArray = this.users.filter(function(item){
+      //We test each element of the object to see if one string matches the regexp.
+      return (myRegex.test(item.date) || myRegex.test(item.name) || myRegex.test(item.email) || myRegex.test(item.cellphone) || myRegex.test(item.birthday) ||myRegex.test(item.gender) ||
+      myRegex.test(item.establishment) ||myRegex.test(item.headquart) ||myRegex.test(item.usability) ||myRegex.test(item.quantity) )
+  
+  });
+    
   }
 
 }
