3c6502e1dc30e702df4dbc6ee58d175a3d5ce342
dianasofi <dianasofi@unicauca.edu.co>
2020-03-25 00:09:26 -0500

    new changes accompaniments table and update section

--- src/app/modules/AllyManag/components/accompaniments/accompaniments.component.ts
+++ src/app/modules/AllyManag/components/accompaniments/accompaniments.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { SectionsService  } from "src/app/services/sections.service";
+import { SectionsService } from "src/app/services/sections.service";
 import Swal from 'sweetalert2';
+import { FormGroup, FormBuilder, Validators, FormControl, FormArray, NgForm } from '@angular/forms'
+
 
 @Component({
   selector: 'app-accompaniments',
@@ -8,25 +10,75 @@ import Swal from 'sweetalert2';
   styleUrls: ['./accompaniments.component.scss']
 })
 export class AccompanimentsComponent implements OnInit {
+  addForm: FormGroup;
+
+  rows: FormArray;
+  itemForm: FormGroup;
+
+
+  //Object that saves all the values of the form
+  preAccompaniments: Object = {
+    quantity: null,
+    unitMeasurement: null,
+    name: null,
+    preparationTime: [],
+    numberOfModifications: null,
+    state: null,
+    typeOfAccompaniment: null,
+    accompanimentValue: null,
+    idTypeSection: null,
+    nameTypeSection: null
+  }
 
   //variables for sections
   sections: any[] = [];
+  sectionSelected: String = null;
+  sectiontoUpdate={};
 
+  //variable for preparation time
+  time: String[] = [];
 
+  constructor(private sectionService: SectionsService, private fb: FormBuilder) {
+    //inicialization for the table
+    this.addForm = this.fb.group({
+      items: [null, Validators.required],
+      items_value: ['no', Validators.required]
+    });
+    this.rows = this.fb.array([]);
 
-  constructor(private sectionService: SectionsService) {
-    
-     //inicialization service with collections dishes-categories
-     this.sectionService.getSections().subscribe(section=>{
-       this.sections = section;
-     })
-    }
+    //preparation time
+    this.time = ['minutos', 'horas']
 
-  ngOnInit() {
+    //inicialization service with collections dishes-categories
+    this.sectionService.getSections().subscribe(section => {
+      this.sections = section;
+    })
+
   }
 
-  //Method for add new section
-  addSection(name: String){
+  ngOnInit() {
+    this.addForm.get("items").valueChanges.subscribe(val => {
+      if (val === true) {
+        this.addForm.get("items_value").setValue("yes");
+
+        this.addForm.addControl('rows', this.rows);
+      }
+      if (val === false) {
+        this.addForm.get("items_value").setValue("no");
+        this.addForm.removeControl('rows');
+      }
+    });
+  }
+
+  //Method to set the section selected
+  seeValue(name: String,id:String) {
+    this.sectionSelected = name;
+    this.sectiontoUpdate ={name:name,id:id}
+    this.preAccompaniments['nameTypeSection'] = this.sectionSelected
+  }
+
+  //CRD -- Methos of sections: CREATE ,READ, UPDATE AND DELETE 
+  addSection(name: String) {
     let newitem = name;
     let newCategory: object = {
       name: newitem
@@ -34,8 +86,44 @@ export class AccompanimentsComponent implements OnInit {
     this.swallSaveOtherSection(newCategory)
   }
 
+  updateSection(sectionUpdated:String) {
+    let itemUpdated = sectionUpdated;
+    let sectionEdited: object = {
+      name: itemUpdated
+    }
+    this.swallUpdateSection(this.sectiontoUpdate,sectionEdited)
+  }
+
+  deleteSection() {
+    this.swallDeleteSection(this.sectionSelected)
+  }
+
+  //Methods for the table add, remove and create item
+  onAddRow() {
+    this.rows.push(this.createItemFormGroup());
+  }
+
+  onRemoveRow(rowIndex: number) {
+    this.rows.removeAt(rowIndex);
+  }
+
+  createItemFormGroup(): FormGroup {
+    return this.fb.group({
+      quantity: null,
+      unitMeasurement: null,
+      name: null,
+      preparationTime: [],
+      numberOfModifications: null,
+      state: null,
+      typeOfAccompaniment: null,
+      accompanimentValue: null,
+      idTypeSection: null,
+      nameTypeSection: null
+    });
+  }
+
   //Sweet alert for adding a new section
-  swallSaveOtherSection(newCategory:any){
+  swallSaveOtherSection(newCategory: any) {
     Swal.fire({
       title: 'Estás seguro?',
       text: "de que deseas guardar esta nueva sección!",
@@ -47,7 +135,7 @@ export class AccompanimentsComponent implements OnInit {
     }).then((result) => {
       if (result.value) {
         this.sectionService.postSection(newCategory).subscribe(() => {
-          this.sectionService.getSections().subscribe(section=>{
+          this.sectionService.getSections().subscribe(section => {
             this.sections = section;
           })
         })
@@ -60,4 +148,62 @@ export class AccompanimentsComponent implements OnInit {
     })
   }
 
+  swallUpdateSection(sectionSelected: any, newCategory: any) {
+    Swal.fire({
+      title: 'Estás seguro?',
+      text: "de que deseas actualizar el nombre de esta sección!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#542b81',
+      cancelButtonColor: '#542b81',
+      confirmButtonText: 'Si, actualizar!'
+    }).then((result) => {
+      if (result.value) {
+        this.sectionService.putSection(sectionSelected.id,newCategory).subscribe(res=>{
+          this.sectionService.getSections().subscribe(section => {
+            this.sections = section})
+        })
+        Swal.fire(
+          'Actualizado!',
+          'success',
+        )
+      }
+    })
+  }
+
+
+  swallDeleteSection(sectionSelected: String) {
+    Swal.fire({
+      title: 'Estás seguro?',
+      text: "de que deseas eliminar esta sección!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#542b81',
+      cancelButtonColor: '#542b81',
+      confirmButtonText: 'Si, eliminar!'
+    }).then((result) => {
+      if (result.value) {
+        this.sectionService.getSections().subscribe(section => {
+          this.sections = section;
+          this.sections.forEach((element: any) => {
+            let sec: any = {
+              id: element.id,
+              name: element.name,
+            }
+            if (sec.name == sectionSelected) {
+              this.sectionService.deleteSection(sec.id).subscribe(() => {
+                this.sectionService.getSections().subscribe(sections => {
+                  this.sections = sections;
+                })
+              })
+            }
+          })
+        })
+        Swal.fire(
+          'Eliminado!',
+          'success',
+        )
+      }
+    })
+  }
 }
