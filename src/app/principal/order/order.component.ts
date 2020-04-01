4a093c934954668d7638ab77ed3022a7e3f3d640
aldairl <aldairl@unicauca.edu.co>
2020-04-01 00:18:06 -0500

    cronometer ready for orders

--- src/app/principal/order/order.component.ts
+++ src/app/principal/order/order.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { OrderByUser } from 'src/app/models/OrderByUser';
 
 @Component({
   selector: 'app-order',
@@ -8,13 +9,151 @@ import { Component, OnInit } from '@angular/core';
 export class OrderComponent implements OnInit {
 
   expresionColor = 'blue';
+  showdetail = false;
+  startCronometer = true;
+  timeToralOut = '';
+  stopOrder = false;
+
+  @Input()
+  order: OrderByUser = {};
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.startCronometer);
+
+
+
+    // let precronometer = setInterval(() => {
+
+    //   this.InitCronometer();
+
+    //   if (this.stopOrder) {
+    //     clearInterval(precronometer);
+    //   }
+
+    // }, 1000);
+
+
+
+    // let postcronometer = setInterval(() => {
+
+    //   this.cronometer();
+
+    //   if (this.startCronometer) {
+    //     console.log("limpando");
+
+    //     clearInterval(postcronometer);
+    //   }
+
+    // }, 1000);
+
+
   }
 
-  showDetail(){
+  showDetail() {
     console.log("clic en item");
+    this.showdetail = !this.showdetail;
   }
+
+  cronometer() {
+
+    // datedelivery = 31/3/2020
+    let datedelivery = new Date(this.order.DateDelivery).toLocaleString('es-ES', { day: '2-digit', month: 'numeric', year: 'numeric' });
+    // today = 31/3/2020
+    let today = new Date().toLocaleString('es-ES', { day: '2-digit', month: 'numeric', year: 'numeric' });
+
+    // hours
+    // time today = 4:21 PM
+    let timeToday = new Date().toLocaleString('en-US', { hour: 'numeric', minute: '2-digit', hour12: true });
+    let timeDelivery = new Date(this.order.DateDelivery).toLocaleString('en-US', { hour: 'numeric', minute: '2-digit', hour12: true });
+
+    // timecronometer = 30 minutos || 1:15 min
+    let timeCronometer = this.order.timeTotal;
+
+    // console.log( "comparando", datedelivery, today);
+
+    // console.log("time cronometer", timeCronometer);
+
+    let hourtoday = parseInt(timeToday.split(" ")[0].split(":")[0]);
+    let minutstoday = parseInt(timeToday.split(" ")[0].split(":")[1]);
+
+    let hoursCronometer = 0;
+    let minutsCronometer = 0;
+
+    if (datedelivery == today) {
+
+      switch (timeCronometer.split(" ")[1]) {
+
+        case 'min':
+          hoursCronometer = parseInt(timeCronometer.split(" ")[0].split(":")[0]);
+          minutsCronometer = parseInt(timeCronometer.split(" ")[0].split(":")[1]);
+          break;
+
+        case 'minutos':
+          minutsCronometer = parseInt(timeCronometer.split(" ")[0]);
+          break
+      }
+
+      if ((minutstoday + minutsCronometer) > 59) {
+        hoursCronometer += 1;
+        minutsCronometer = 0;
+      }
+      // hour to compare with hour delivery 
+      let hourCompare = (hourtoday + hoursCronometer) + ":" + (minutstoday + minutsCronometer) + " " + timeDelivery.split(" ")[1];
+      console.log("comparando", hourCompare, timeDelivery);
+
+      if (hourCompare === timeDelivery) {
+        console.log("son iguales");
+
+        this.startCronometer = true;
+        console.log(this.startCronometer);
+
+      }
+
+    }
+  }
+
+
+  InitCronometer() {
+
+    if (this.startCronometer) {
+
+      // timecronometer = 30 minutos || 1:15 min
+      let timeCronometer = this.order.timeTotalCronometer;
+      
+      let hoursCronometer = 0;
+      let minutsCronometer = 0;
+
+      let seconds = new Date().getSeconds();
+      
+      if ( seconds >= 59 ){
+
+        switch (timeCronometer.split(" ")[1]) {
+  
+          case 'min':
+            minutsCronometer = parseInt(timeCronometer.split(" ")[0].split(":")[1]);
+            minutsCronometer -= 1;
+            hoursCronometer = parseInt(timeCronometer.split(" ")[0].split(":")[0]);
+            hoursCronometer =  minutsCronometer == 0 ? hoursCronometer -= 1 : hoursCronometer ;
+            this.timeToralOut = hoursCronometer + ':' + minutsCronometer + " "+ 'min';
+            break;
+  
+          case 'minutos':
+            minutsCronometer = parseInt(timeCronometer.split(" ")[0]);
+            minutsCronometer -= 1;            
+            this.timeToralOut = minutsCronometer +" "+ 'minutos';
+            break
+        }
+
+        if (hoursCronometer < 0 && minutsCronometer < 0) {
+          this.stopOrder = true;
+        }
+  
+        // asign new cronometer
+        this.order.timeTotalCronometer = this.timeToralOut;
+      }
+    }
+  }
+
 }
--- src/app/principal/principal-orders/principal-orders.component.ts
+++ src/app/principal/principal-orders/principal-orders.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from 'src/app/services/orders.service';
+import { Orders } from 'src/app/models/Orders';
+import { UsersService } from 'src/app/services/users.service';
+import { OrderByUser } from 'src/app/models/OrderByUser';
+import { Users } from 'src/app/models/Users';
+import { DishesService } from 'src/app/services/dishes.service';
+import { Dishes } from 'src/app/models/Dishes';
+import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'app-principal-orders',
@@ -6,52 +14,170 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./principal-orders.component.scss']
 })
 export class PrincipalOrdersComponent implements OnInit {
-  isCollapsed = false;
 
   Hours = []
   Tables = []
 
-  constructor() { 
+  testlist = [
+    {
+      id: 3,
+      DateDelivery: "2020-03-31T23:45:04.671Z"
+    },
+    {
+      id: 4,
+      DateDelivery: "2020-03-27T14:20:04.671Z"
+    },
+    {
+      id: 5,
+      DateDelivery: "2020-03-27T14:20:04.671Z"
+    },
+    {
+      id: 1,
+      DateDelivery: "2020-03-27T14:20:04.671Z"
+    }
+    
+  ];
+
+  testlist2 = [];
+
+  orders = []
+  orders2 = []
+  constructor(private serviceOrders: OrdersService, private userservice: UsersService, private dishService: DishesService) {
     // init hours
     this.createHours();
 
     // init Tables
     this.createTables();
-  }
+    let or = []
+    this.serviceOrders.getOrders().subscribe( async (orders: Orders[]) => {
 
-  ngOnInit() {
-  }
+      await orders.forEach(order => {
 
-  getHour(event){
-    // get the text in button
-    console.log(event.target.textContent);
+        let ordertosave: OrderByUser = {}
+
+        this.userservice.getUserById(order.idUser).subscribe((user: Users) => {
+
+          ordertosave.id = order.code;
+          ordertosave.name = user.name + " " + user.lastname;
+          ordertosave.typeOfService = order.typeOfService;
+          ordertosave.purchaseAmount = order.orderValue;
+          ordertosave.registerDate = this.convertDate(order.dateAndHourReservation);
+          ordertosave.dateAndHourDelivery = this.convertDate(order.dateAndHourDelivey);
+          ordertosave.DateDelivery = order.dateAndHourDelivey;
+
+        })
+
+        let objdishes = [];
+        let timeTotal = 0;
+
+        order.idDishe.forEach((iddish: any) => {
+          let objDish: any = {};
+
+          this.dishService.getDisheById(iddish.id).subscribe((dish: Dishes) => {
+
+            objDish.quantity = iddish.quantity;
+            objDish.name = dish.name;
+            objDish.description = dish.description;
+            objDish.timedish = dish.preparationTime[0] + " " + dish.preparationTime[1];
+            objDish.valueDish = dish.price * iddish.quantity;
+            switch (dish.preparationTime[1]) {
+              case 'segundos':
+                timeTotal = timeTotal + (parseInt(dish.preparationTime[0]) / 60);
+                break;
+
+              case "minutos":
+                timeTotal = (timeTotal + parseInt(dish.preparationTime[0]));
+                break;
+
+              case 'horas':
+                timeTotal = timeTotal + (parseInt(dish.preparationTime[0]) * 60);
+                break;
+            }
+
+            if (timeTotal >= 60) {
+
+              ordertosave.timeTotal = Math.floor(timeTotal / 60) + ":" + ((((timeTotal / 60) - Math.floor(timeTotal / 60)) * 60) < 10 ? '0' +
+                (((timeTotal / 60) - Math.floor(timeTotal / 60)) * 60) : (((timeTotal / 60) - Math.floor(timeTotal / 60)) * 60)) + " " + "min";
+
+              ordertosave.timeTotalCronometer = Math.floor(timeTotal / 60) + ":" + ((((timeTotal / 60) - Math.floor(timeTotal / 60)) * 60) < 10 ? '0' +
+                (((timeTotal / 60) - Math.floor(timeTotal / 60)) * 60) : (((timeTotal / 60) - Math.floor(timeTotal / 60)) * 60)) + " " + "min";
+
+            } else {
+              ordertosave.timeTotal = timeTotal + " " + "minutos";
+              ordertosave.timeTotalCronometer = timeTotal + " " + "minutos";
+            }
+          })
+          objdishes.push(objDish);
+
+        })
+
+        ordertosave.nameDishe = objdishes;
+
+        this.orders.push(ordertosave);
+      })
+
+    })
     
   }
 
-  createHours(){
-    for (let hour = 0; hour < 24; hour++) {
-      for (let min = 0; min < 31; min+=30) {
-        let toHour = ''
-        if (hour < 12){
-          toHour = hour < 10 ? "0"+ hour + ":" + (min < 30 ? '0' + min : min) + " am" : hour + ":" + (min < 30 ? '0' + min : min) + " am" ;
-        }else{
-          toHour = (hour - 12 && hour - 12 < 10 )? '0' + (hour - 12) + ":" + (min < 30 ? '0' + min : min) + " pm" : (hour - 12 > 0 ? (hour - 12) : hour  ) 
-          + ":" + (min < 30 ? '0' + min : min) + " pm";
-        }
+  ngOnInit() {
 
+    // this.orderList();
+    this.orders2 = this.orders;
+    
+  }
+
+  getHour(event) {
+    // get the text in button
+    console.log(event.target.textContent);
+
+  }
+
+  createHours() {
+    for (let hour = 0; hour < 24; hour++) {
+      for (let min = 0; min < 31; min += 30) {
+        let toHour = ''
+        if (hour < 12) {
+          toHour = hour < 10 ? "0" + hour + ":" + (min < 30 ? '0' + min : min) + " am" : hour + ":" + (min < 30 ? '0' + min : min) + " am";
+        } else {
+          toHour = (hour - 12 && hour - 12 < 10) ? '0' + (hour - 12) + ":" + (min < 30 ? '0' + min : min) + " pm" : (hour - 12 > 0 ? (hour - 12) : hour)
+            + ":" + (min < 30 ? '0' + min : min) + " pm";
+        }
         this.Hours.push(toHour);
       }
-      
     }
   }
 
-  createTables(){
-
+  createTables() {
     for (let t = 1; t < 21; t++) {
-      
-      this.Tables.push(t)
-      
+      this.Tables.push(t);
     }
-
   }
+
+  convertDate(date: Date): string {
+    const d = new Date(date);
+    const n = d.toLocaleString('es-ES', { day: '2-digit', month: 'numeric', year: 'numeric' }) + " " +
+      d.toLocaleString('en-US', { hour: 'numeric', minute: '2-digit', hour12: true });
+    // const n = date.toLocaleString('en-US', { hour12: true });
+    return n;
+  }
+
+  orderList() {
+
+    this.testlist2 = this.testlist.sort( function (a,b){
+      return new Date(a.DateDelivery).getTime() - new Date(b.DateDelivery).getTime();
+    }
+    );
+    console.log("on funtion", this.orders2);
+    console.log("on funtion", this.testlist2);
+
+    this.orders2 = this.orders.sort( function (a,b){
+      return new Date(a.DateDelivery).getTime() - new Date(b.DateDelivery).getTime();
+    }
+    );
+    
+  }
+
+
+
 }
