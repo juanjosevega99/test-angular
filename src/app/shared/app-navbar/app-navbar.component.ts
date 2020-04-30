4392b57e8c436cabd53e60f986f91115f662bfb3
aldairl <aldairl@unicauca.edu.co>
2020-04-30 17:35:22 -0500

    ready to create a component to notificationorders

--- src/app/shared/app-navbar/app-navbar.component.ts
+++ src/app/shared/app-navbar/app-navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ShowContentService } from 'src/app/services/providers/show-content.service';
 import { profileStorage } from 'src/app/models/ProfileStorage';
+import { OrdersService } from 'src/app/services/orders.service';
+import { Orders } from 'src/app/models/Orders';
 
 @Component({
   selector: 'app-app-navbar',
@@ -13,8 +15,11 @@ export class AppNavbarComponent implements OnInit {
   time:String;
   today:Date;
   profile : profileStorage = new profileStorage();
-  constructor( private showcontentservice: ShowContentService ) { 
+
+  ordersNotifications = 0;
+  constructor( private showcontentservice: ShowContentService, private serviceOrders: OrdersService ) { 
     this.profile =  this.showcontentservice.showMenus();
+    this.loadNotificationsOrders();
   }
 
   ngOnInit() {
@@ -27,5 +32,15 @@ export class AppNavbarComponent implements OnInit {
     this.time = this.today.toLocaleString('en-US',{hour:'numeric',minute:'2-digit',hour12:true});
     this.date = this.today.toLocaleString('es-ES',{weekday:'short',day:'2-digit',month:'long',year:'numeric'});
   }
+
+  loadNotificationsOrders(){
+    this.serviceOrders.getOrdersByAllyHead(this.profile.idHeadquarter).subscribe((orders: Orders[]) => {
+
+      let numorders = orders.filter( order => order.orderStatus == 'pending' );
+      this.ordersNotifications = numorders.length;
+
+    })
+  }
+
 }
 
